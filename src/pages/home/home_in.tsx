3ec1a25e4fe72fbac163577b4ce7ff8048f33015
hsdgac <201624926+hsdgac@users.noreply.github.com>
2025-05-07 16:18:01 +0900

    feat: 홈화면 수정

--- src/pages/home/home_in.tsx
+++ src/pages/home/home_in.tsx
@@ -1,23 +1,24 @@
 // src/pages/home/index.tsx
 import { useState } from "react";
+import { FaPen } from "react-icons/fa";
 
 const categories = ["전체", "채소/과일", "축산/계란류", "수산/건어물", "냉동/간편식품", "음료/유제품", "곡물/면/양념/분말"] as const;
 
 const mockItems = [
-    { id: 1, title: "청정원 춘장 1kg", imageUrl: "/Cow.png" },
-    { id: 2, title: "롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png" },
-    { id: 3, title: "청정원 춘장 1kg", imageUrl: "/Cow.png" },
-    { id: 4, title: "롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png" },
-    { id: 5, title: "청정원 춘장 1kg", imageUrl: "/ender_pearl.png" },
-    { id: 6, title: "롯데 스파게티면 3kg", imageUrl: "/Cow.png" },
-    { id: 7, title: "청정원 춘장 1kg", imageUrl: "/ender_pearl.png" },
-    { id: 8, title: "롯데 스파게티면 3kg", imageUrl: "/Cow.png" },
-] as const;
+    { id: 1, title: "[1] 청정원 춘장 1kg", imageUrl: "/Cow.png", _category: "축산/계란류" },
+    { id: 2, title: "[2] 롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png", _category: "축산/계란류" },
+    { id: 3, title: "[3] 청정원 춘장 1kg", imageUrl: "/Cow.png", _category: "수산/건어물" },
+    { id: 4, title: "[4] 롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png", _category: "음료/유제품" },
+    { id: 5, title: "[5] 청정원 춘장 1kg", imageUrl: "/ender_pearl.png", _category: "축산/계란류" },
+    { id: 6, title: "[6] 롯데 스파게티면 3kg", imageUrl: "/Cow.png", _category: "수산/건어물" },
+    { id: 7, title: "[7] 청정원 춘장 1kg", imageUrl: "/ender_pearl.png", _category: "음료/유제품" },
+    { id: 8, title: "[8] 롯데 스파게티면 3kg", imageUrl: "/Cow.png", _category: "음료/유제품" },
+];
 
 export default function HomePage() {
     const [selectedCategory, setSelectedCategory] = useState("전체");
     return (
-        <div className="">
+        <div className="w-[95%] m-auto">
             <div className="relative flex flex-row justify-between items-center h-24">
                 {/* 상단 제목 */}
                 <h1 className="absolute text-3xl font-bold text-white w-full text-center py-4">식 상하지 않은 재료</h1>
@@ -25,10 +26,10 @@ export default function HomePage() {
                 <SidebarMenu />
             </div>
 
-            <div className="relative w-[1280px] m-auto h-[60rem] bg-white flex flex-col items-center p-6 rounded-3xl">
+            <div id="box" className="relative w-[100%] h-[65rem] m-auto bg-white flex flex-col items-center p-6 rounded-3xl">
                 {/* 카테고리 필터 */}
-                <div className="relative flex justify-center items-center mt-4 border-2 rounded-md border-[#76bf41] p-4">
-                    <h1 className="absolute left-[20px] bottom-[80%] w-28 h-8 rounded-lg bg-[#76bf41]  text-white text-xl font-bold flex items-center justify-center">
+                <div className="relative flex justify-center items-center mt-4 border-2 border-[#76bf41] p-4">
+                    <h1 className="absolute left-[20px] bottom-[80%] w-28 h-8 rounded-2xl bg-[#76bf41]  text-white text-xl font-bold flex items-center justify-center">
                         골라보기
                     </h1>
 
@@ -38,7 +39,9 @@ export default function HomePage() {
                             <button
                                 key={cat}
                                 onClick={() => setSelectedCategory(cat)}
-                                className={`px-3 py-1 rounded-full border ${selectedCategory === cat ? "bg-[#76bf41] text-white" : "bg-white text-[#76bf41]"}`}
+                                className={`px-3 py-1 rounded-full border ${
+                                    selectedCategory === cat ? "text-black underline font-bold text-base" : "text-gray-800 underline text-base"
+                                }`}
                             >
                                 {cat}
                             </button>
@@ -46,7 +49,7 @@ export default function HomePage() {
                     </div>
                 </div>
                 {/* 카드 리스트 */}
-                <CardList />
+                <CardList selectedCategory={selectedCategory} />
                 {/* 우측 하단 버튼 */}
                 <NewPost />
             </div>
@@ -54,34 +57,50 @@ export default function HomePage() {
     );
 }
 
-function CardList() {
+function CardList({ selectedCategory }: { selectedCategory: string }) {
     return (
         <div className="grid grid-cols-4 gap-12 mt-20 justify-center w-full ">
-            {mockItems.map((item) => (
-                <div key={item.id} className="bg-white rounded-lg border-2 shadow-md overflow-hidden flex flex-col items-center p-4">
-                    <img src={item.imageUrl} alt={item.title} className="w-32 h-32 object-cover" />
-                    <p className="mt-2 text-sm text-center">{item.title}</p>
-                </div>
-            ))}
+            {selectedCategory === "전체" &&
+                mockItems.map((item) => (
+                    <div key={item.id} className="bg-white rounded-lg border-2 shadow-md overflow-hidden flex flex-col items-center p-4">
+                        <img src={item.imageUrl} alt={item.title} className="w-32 h-32 object-cover" />
+                        <p className="mt-2 text-sm text-center">{item.title}</p>
+                    </div>
+                ))}
+            {selectedCategory !== "전체" &&
+                mockItems
+                    .filter((item) => item._category === selectedCategory)
+                    .map((item) => (
+                        <div key={item.id} className="bg-white rounded-lg border-2 shadow-md overflow-hidden flex flex-col items-center p-4">
+                            <img src={item.imageUrl} alt={item.title} className="w-32 h-32 object-cover" />
+                            <p className="mt-2 text-sm text-center">{item.title}</p>
+                        </div>
+                    ))}
         </div>
     );
 }
 
 function NewPost() {
-    return <button className="absolute bottom-8 right-8 bg-[#76bf41] text-white px-3 py-1 rounded-md shadow-xl font-semibold">✎ 글쓰기</button>;
+    return (
+        <button className="absolute bottom-8 right-8 bg-[#76bf41] text-white px-3 py-1 rounded-md shadow-xl font-semibold">
+            <FaPen className="inline align-middle" /> 글쓰기
+        </button>
+    );
 }
 
 function SidebarMenu() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="flex ml-auto h-16 z-0">
+        <div className="flex ml-auto h-16 z-10">
             {/* 햄버거 버튼 */}
-            <button className={`top-8 right-16 p-2`} onClick={() => setIsOpen((prev) => !prev)}>
-                <div className="w-8 h-1 bg-white mb-1"></div>
-                <div className="w-8 h-1 bg-white mb-1"></div>
-                <div className="w-8 h-1 bg-white"></div>
-            </button>
+            <div className="w-full flex flex-row-reverse">
+                <div className="flex flex-col gap-1 p-5 cursor-pointer" onClick={() => setIsOpen((prev) => !prev)}>
+                    <div className="w-[32px] bg-[#FFF] h-[7px] rounded-md"></div>
+                    <div className="w-[32px] bg-[#FFF] h-[7px] rounded-md"></div>
+                    <div className="w-[32px] bg-[#FFF] h-[7px] rounded-md"></div>
+                </div>
+            </div>
 
             {/* 사이드 메뉴 */}
             <div
--- src/pages/home/home_out.tsx
+++ src/pages/home/home_out.tsx
@@ -1,23 +1,25 @@
 // src/pages/home/index.tsx
 import { useState } from "react";
+import { FaPen } from "react-icons/fa";
+import { RiKakaoTalkFill } from "react-icons/ri";
 
 const categories = ["전체", "채소/과일", "축산/계란류", "수산/건어물", "냉동/간편식품", "음료/유제품", "곡물/면/양념/분말"] as const;
 
 const mockItems = [
-    { id: 1, title: "청정원 춘장 1kg", imageUrl: "/Cow.png" },
-    { id: 2, title: "롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png" },
-    { id: 3, title: "청정원 춘장 1kg", imageUrl: "/Cow.png" },
-    { id: 4, title: "롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png" },
-    { id: 5, title: "청정원 춘장 1kg", imageUrl: "/ender_pearl.png" },
-    { id: 6, title: "롯데 스파게티면 3kg", imageUrl: "/Cow.png" },
-    { id: 7, title: "청정원 춘장 1kg", imageUrl: "/ender_pearl.png" },
-    { id: 8, title: "롯데 스파게티면 3kg", imageUrl: "/Cow.png" },
-] as const;
+    { id: 1, title: "[1] 청정원 춘장 1kg", imageUrl: "/Cow.png", _category: "축산/계란류" },
+    { id: 2, title: "[2] 롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png", _category: "축산/계란류" },
+    { id: 3, title: "[3] 청정원 춘장 1kg", imageUrl: "/Cow.png", _category: "수산/건어물" },
+    { id: 4, title: "[4] 롯데 스파게티면 3kg", imageUrl: "/ender_pearl.png", _category: "음료/유제품" },
+    { id: 5, title: "[5] 청정원 춘장 1kg", imageUrl: "/ender_pearl.png", _category: "축산/계란류" },
+    { id: 6, title: "[6] 롯데 스파게티면 3kg", imageUrl: "/Cow.png", _category: "수산/건어물" },
+    { id: 7, title: "[7] 청정원 춘장 1kg", imageUrl: "/ender_pearl.png", _category: "음료/유제품" },
+    { id: 8, title: "[8] 롯데 스파게티면 3kg", imageUrl: "/Cow.png", _category: "음료/유제품" },
+];
 
 export default function HomePage() {
     const [selectedCategory, setSelectedCategory] = useState("전체");
     return (
-        <div className="w-[1280px] m-auto">
+        <div className="w-[95%] m-auto">
             <div className="relative flex flex-row justify-between items-center h-24">
                 {/* 상단 제목 */}
                 <h1 className="absolute text-3xl font-bold text-white w-full text-center py-4 rounded-lg">식 상하지 않은 재료</h1>
@@ -25,10 +27,10 @@ export default function HomePage() {
                 <KakaoLoginButton />
             </div>
 
-            <div className="relative w-[1280px] m-auto h-[60rem] bg-white flex flex-col items-center p-6 rounded-3xl">
+            <div className="relative w-[100%] h-[65rem] m-auto bg-white flex flex-col items-center p-6 rounded-3xl">
                 {/* 카테고리 필터 */}
-                <div className="relative flex justify-center items-center mt-4 border-2 rounded-md border-[#76bf41] p-4">
-                    <h1 className="absolute left-[20px] bottom-[80%] w-28 h-8 rounded-lg bg-[#76bf41]  text-white text-xl font-bold flex items-center justify-center">
+                <div className="relative flex justify-center items-center mt-4 border-2 border-[#76bf41] p-4">
+                    <h1 className="absolute left-[20px] bottom-[80%] w-28 h-8 rounded-2xl bg-[#76bf41]  text-white text-xl font-bold flex items-center justify-center">
                         골라보기
                     </h1>
 
@@ -38,7 +40,9 @@ export default function HomePage() {
                             <button
                                 key={cat}
                                 onClick={() => setSelectedCategory(cat)}
-                                className={`px-3 py-1 rounded-full border ${selectedCategory === cat ? "bg-[#76bf41] text-white" : "bg-white text-[#76bf41]"}`}
+                                className={`px-3 py-1 rounded-full border ${
+                                    selectedCategory === cat ? "text-black underline font-bold text-base" : "text-gray-800 underline text-base"
+                                }`}
                             >
                                 {cat}
                             </button>
@@ -46,7 +50,7 @@ export default function HomePage() {
                     </div>
                 </div>
                 {/* 카드 리스트 */}
-                <CardList />
+                <CardList selectedCategory={selectedCategory} />
                 {/* 우측 하단 버튼 */}
                 <NewPost />
             </div>
@@ -55,22 +59,40 @@ export default function HomePage() {
 }
 
 function KakaoLoginButton() {
-    return <button className="ml-auto bg-yellow-400 text-black px-4 py-2 rounded-full font-bold cursor-pointer z-0">💬 카카오로 로그인</button>;
+    return (
+        <button className="ml-auto bg-yellow-400 text-black px-4 py-2 rounded-full font-bold cursor-pointer z-0">
+            <RiKakaoTalkFill /> 카카오로 로그인
+        </button>
+    );
 }
 
-function CardList() {
+function CardList({ selectedCategory }: { selectedCategory: string }) {
     return (
         <div className="grid grid-cols-4 gap-12 mt-20 justify-center w-full ">
-            {mockItems.map((item) => (
-                <div key={item.id} className="bg-white rounded-lg border-2 shadow-md overflow-hidden flex flex-col items-center p-4">
-                    <img src={item.imageUrl} alt={item.title} className="w-32 h-32 object-cover" />
-                    <p className="mt-2 text-sm text-center">{item.title}</p>
-                </div>
-            ))}
+            {selectedCategory === "전체" &&
+                mockItems.map((item) => (
+                    <div key={item.id} className="bg-white rounded-lg border-2 shadow-md overflow-hidden flex flex-col items-center p-4">
+                        <img src={item.imageUrl} alt={item.title} className="w-32 h-32 object-cover" />
+                        <p className="mt-2 text-sm text-center">{item.title}</p>
+                    </div>
+                ))}
+            {selectedCategory !== "전체" &&
+                mockItems
+                    .filter((item) => item._category === selectedCategory)
+                    .map((item) => (
+                        <div key={item.id} className="bg-white rounded-lg border-2 shadow-md overflow-hidden flex flex-col items-center p-4">
+                            <img src={item.imageUrl} alt={item.title} className="w-32 h-32 object-cover" />
+                            <p className="mt-2 text-sm text-center">{item.title}</p>
+                        </div>
+                    ))}
         </div>
     );
 }
 
 function NewPost() {
-    return <button className="absolute bottom-8 right-8 bg-[#76bf41] text-white px-3 py-1 rounded-md shadow-xl font-semibold">✎ 글쓰기</button>;
+    return (
+        <button className="absolute bottom-8 right-8 bg-[#76bf41] text-white px-3 py-1 rounded-md shadow-xl font-semibold">
+            <FaPen className="inline align-middle" /> 글쓰기
+        </button>
+    );
 }
